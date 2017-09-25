cd8f78498508c286e42b4f76144c2acb4261b585
chai <1576654302@qq.com>
2017-09-25 18:23:09 +0800

    add css

---
+++ news/resources/views/vote/home.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>投票</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="banner">
+    <div>
+        <img src="http://www.news.com/img/ydlb-1.jpg" alt="">
+    </div>
+</div>
+<div class="count_number">
+    <ul>
+        <li><p>参与选手</p><span>30</span></li>
+        <li><p>累计投票</p><span>100256</span></li>
+        <li><p>访问量</p><span>1025468</span></li>
+    </ul>
+    <div class="clear"></div>
+</div>
+
+<div class="countdown">
+    <ul>
+        <li class="clock"></li>
+        <li>
+            <p>距离活动结束还有：</p>
+            <p>860 天 03 时 08 分 21 秒</p>
+        </li>
+    </ul>
+    <div class="clear"></div>
+</div>
+
+<div class="containers">
+    <div class="introduce">
+        <div><span>tt</span>投票测试</div>
+        <div><span>tm</span>2017-10-01至2017-10-05</div>
+        <div><span>ts</span>活动介绍</div>
+    </div>
+    <div class="list">
+        <div class="ls_tp">
+            <a href="">
+                <img src="http://www.news.com/img/dyd.jpg" alt="" width="180">
+            </a>
+            <div class="username">14号 获悉</div>
+            <div class="toup">
+                <span class="like"></span>
+                <button type="button">投票</button>
+                <span>5456票</span>
+            </div>
+        </div>
+
+        <div class="ls_tp">
+            <a href="">
+                <img src="http://www.news.com/img/ys.jpg" alt="" width="180">
+            </a>
+            <div class="username">14号 获悉</div>
+            <div class="toup">
+                <span class="like"></span>
+                <button type="button">投票</button>
+                <span>5456票</span>
+            </div>
+        </div>  
+        <div class="clear"></div>      
+    </div>
+
+</div>
+<div class="footer">
+    <nav>
+        <a href=""><p>home</p><span>首页</span></a>
+        <a href=""><p>tel</p><span>咨询</span></a>
+        <a href=""><p>home</p><span>搜</span></a>
+        <a href=""><p>ser</p><span>排行</span></a>
+        <a href=""><p>lap</p><span>我要拉票</span></a>
+    </nav>
+</div>
+</body>
+</html>
